355739053c4a6cc5acda09e37d544ed181d4591b
Andrey <andrey.mikhaylov.n@gmail.com>
2024-11-14 12:52:20 +0300

    class, test2, resize, paste

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 from PIL import Image
 
+
+class Pic:
+    def __init__(self, path):
+        self.img = Image.open(path)
+
+    def print_info(self):
+        print(self.img.format, self.img.mode, self.img.size)
+
+    def crop(self, left, upper, wdt, hgt):
+        self.img = self.img.crop((left, upper, left+wdt, upper+hgt))
+
+    def resize(self, new_w, new_h):
+        self.img = self.img.resize((int(new_w), int(new_h)))
+
+    def width(self):
+        return self.img.width
+
+    def height(self):
+        return self.img.height
+
+    def show(self):
+        self.img.show()
+
+    def save(self, path):
+        self.img.save(path)
+
+    def paste(self, other, x, y):
+        self.img.paste(other.img, (x, y))
+
+
 def test1():
     #img = Image.new('RGB', (200, 200), (255, 255, 0))
     #img = Image.open("images.jpg")
@@ -10,9 +40,25 @@ def test1():
     img2.show()
     img2.save("out.jpg")
 
+def test2():
+    img = Pic("high.webp")
+    img.print_info()
+    img.crop(16, 418, 650, 493)
+    img.print_info()
+    #img.show()
+    #img.save("out.jpg")
+
+    img2 = Pic("images.jpg")
+    img2.resize(img2.width()/2, img2.height()/2)
+    img.paste(img2, 530, 5)
+    img.print_info()
+    img.show()
+    img.save("out.jpg")
+
 
 if __name__ == '__main__':
-    test1()
+    #test1()
+    test2()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
